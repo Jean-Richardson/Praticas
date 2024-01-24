d93e4a4081f0b5084b155ba4301cc078a7c2b070
Jean Gonlçalves <124441400+Jean-Richardson@users.noreply.github.com>
2024-01-24 14:50:25 -0300

    Update questao2.c

--- Listas/Revisao/questao2.c
+++ Listas/Revisao/questao2.c
@@ -4,8 +4,8 @@
 
 char *concatenarStrings(const char *str1, const char *str2) {
   size_t len_str1 = strlen(str1);
-  size_t len_str2 = strlen(str2);
-  size_t len_total = len_str1 + len_str2 + 1; // +1 para o caractere nulo
+   size_t len_str2 = strlen(str2);
+    size_t len_total = len_str1 + len_str2 + 1; // +1 para o caractere nulo
 
   char *novaString = (char *)malloc(len_total * sizeof(char));
 
@@ -24,17 +24,17 @@ int main() {
   char primeiraString[100];  
   char segundaString[100];   
 
-  printf("Insira a primeira string: ");
-  scanf(" %[^\n]", primeiraString);
+   printf("Insira a primeira string: ");
+     scanf(" %[^\n]", primeiraString);
 
-  printf("Insira a segunda string: ");
-  scanf(" %[^\n]", segundaString);
+      printf("Insira a segunda string: ");
+        scanf(" %[^\n]", segundaString);
 
   char *resultado = concatenarStrings(primeiraString, segundaString);
 
-  printf("Resultado da concatenação: %s\n", resultado);
+   printf("Resultado da concatenação: %s\n", resultado);
 
-  free(resultado);
+    free(resultado);
 
   return 0;
 }
