506ece866ba1e9eee00f15a070655356952b4bb1
Jean Gonlçalves <124441400+Jean-Richardson@users.noreply.github.com>
2024-01-28 17:45:17 -0300

    Update questao1_union.c

--- Listas/exercicio_enum/questao1_union.c
+++ Listas/exercicio_enum/questao1_union.c
@@ -14,24 +14,24 @@ int idade;
 sexo gen;
 }Pessoa;
 
-   void preencher(Pessoa * p){
+void preencher(Pessoa * p){
      printf("Qual o seu nome?:");
-        scanf(" %[^\n]", p->nome);
+     scanf(" %[^\n]", p->nome);
 
-          printf("Qual a sua idade?:");
-        scanf("%d", &p->idade);
+     printf("Qual a sua idade?:");
+     scanf("%d", &p->idade);
 }
 
 void imprimir_macho(Pessoa * p){
-  printf("O nome: %s\n", p->nome);
-    printf("A idade: %d\n", p->idade);
-      printf("O sexo: %s", p->gen.masculino);
+     printf("O nome: %s\n", p->nome);
+     printf("A idade: %d\n", p->idade);
+     printf("O sexo: %s", p->gen.masculino);
 }
 
 void imprimir_femea(Pessoa * p){
-  printf("O nome: %s\n", p->nome);
-    printf("A idade: %d\n", p->idade);
-      printf("O sexo: %s", p->gen.feminino);
+     printf("O nome: %s\n", p->nome);
+     printf("A idade: %d\n", p->idade);
+     printf("O sexo: %s", p->gen.feminino);
 }
 
 int main(){
@@ -49,11 +49,11 @@ printf("Digite 1 para masculino ou 0 para feminino:\n");
 
   if(op){
      strcpy(pessoas->gen.masculino,"Masculino");
-    imprimir_macho(pessoas);
+     imprimir_macho(pessoas);
   }
-  else{
- strcpy(pessoas->gen.feminino,"Feminino");
-imprimir_femea(pessoas);
+    else{
+     strcpy(pessoas->gen.feminino,"Feminino");
+     imprimir_femea(pessoas);
   ;}
   
   free(pessoas);
